af163cc1beee60908c2196fb1d57cda78e6004a9
Alhajideen <salifunurudeen834@gmail.com>
2022-11-01 16:24:56 -0600

    update handleChange to get and update previous state

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -22,8 +22,15 @@ class TodoContainer extends React.Component {
       },
     ],
   };
-  handleChange = () => {
-    console.log('clicked');
+  handleChange = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    }));
   };
   render() {
     return (
