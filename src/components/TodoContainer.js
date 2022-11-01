f6958fa8d9c11551afd28ad173684d77f6abc17e
Alhajideen <salifunurudeen834@gmail.com>
2022-11-01 16:08:37 -0600

    get handleChange prop in Todolist

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -29,7 +29,10 @@ class TodoContainer extends React.Component {
     return (
       <div>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+        />
       </div>
     );
   }
